79e47edc0afb1425ebb82dfc4cb96d2f5ccffd74
Mostafa El Sharkawy <elsharkawy.mostafa20@gmail.com>
2025-08-19 20:25:05 +0300

    finished admin functionality

--- SupermarketWebApp/Controllers/AdminController.cs
+++ SupermarketWebApp/Controllers/AdminController.cs
@@ -27,5 +27,41 @@ namespace SupermarketWebApp.Controllers
             var products = context.Products.ToList();
             return View("AllProductsAdmin",products);
         }
+
+        public IActionResult UpdateProductPage(int id)
+        {
+            ViewBag.Categories = context.Categories.ToList();
+            var product = context.Products.FirstOrDefault(m => m.id == id);
+            return View("UpdateProduct",product);
+        }
+
+        public IActionResult UpdateProduct(Product product)
+        {
+            var existingProduct = context.Products.FirstOrDefault(m => m.id == product.id);
+            if (existingProduct != null)
+            {
+                existingProduct.name = product.name;
+                existingProduct.description = product.description;
+                existingProduct.price = product.price;
+                existingProduct.imagePath = product.imagePath;
+                existingProduct.quantity = product.quantity;
+                existingProduct.categoryId = product.categoryId;
+                existingProduct.imagePath = product.imagePath ?? "https://via.placeholder.com/150"; // Default image path if not provided
+                context.SaveChanges();
+            }
+            return RedirectToAction("allproducts");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = context.Products.FirstOrDefault(m => m.id == id);
+            if (product != null)
+            {
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+            return RedirectToAction("allproducts");
+        }
     }
 }
