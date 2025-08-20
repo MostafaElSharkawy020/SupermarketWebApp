12f4f32848a6f844cbf19f2025e4f75f0dd7cf24
Mostafa El Sharkawy <elsharkawy.mostafa20@gmail.com>
2025-08-20 12:45:03 +0300

    finished login

--- SupermarketWebApp/Controllers/LoginController.cs
+++ SupermarketWebApp/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SupermarketWebApp.Models;
 using SupermarketWebApp.Data;
+using SupermarketWebApp.ViewModels;
 
 namespace SupermarketWebApp.Controllers
 {
@@ -20,6 +21,22 @@ namespace SupermarketWebApp.Controllers
             context.SaveChanges();
             Console.WriteLine(user.firstName + " " + user.lastName);
             return View("Login");
+        }
+
+        public IActionResult LoginPage()
+        {
+            return View("Login");
+        }
+
+        public IActionResult Login(LoginViewModel model) 
+        {
+            var user = context.Users.FirstOrDefault(c => c.email == model.email && c.password ==  model.password);
+            if (user == null)
+            {
+                return RedirectToAction("loginpage");
+            }
+            return RedirectToAction("AllProducts", "Products");
+
         }
     }
 }
--- SupermarketWebApp/ViewModels/LoginViewModel.cs
+++ SupermarketWebApp/ViewModels/LoginViewModel.cs
@@ -2,5 +2,7 @@
 {
     public class LoginViewModel
     {
+        public string email { get; set; }
+        public string password { get; set; }
     }
 }
